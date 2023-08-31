36ac5d1ab9273bf747a3bf48e2cd3f4d837dbe42
Knurenko <bod93_93@mail.ru>
2023-08-31 07:29:25 +0300

    Added detected object entity & overlay view

---
+++ app/src/main/java/com/knurenko/whatsthat/domain/DetectedObject.kt
@@ -0,0 +1,19 @@
+package com.knurenko.whatsthat.domain
+
+import android.graphics.RectF
+
+/**
+ * Entity to represent detected object on the screen
+ *
+ * @param relativeBox Square part of the screen, where is located detected object
+ * each coordinate lays in [0..1] and is value, by which we should multiply w/h of container's view to get exact view borders
+ *
+ * @param label Detected object's label with highest match coefficient (result from ML kit)
+ *
+ * @author Knurenko Bogdan 31.08.2023
+ */
+data class DetectedObject(
+    val id: Int,
+    val relativeBox: RectF,
+    val label: String?
+)
